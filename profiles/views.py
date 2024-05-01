c1f31a7f2325e420ff0aaeb0b1acfbdf9c8e2a71
Chris Diggle <chrisdiggle8@outlook.com>
2024-05-01 11:02:59 +0000

    Added profiles detail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,47 @@
 from rest_framework.views import APIView
+from rest_framework import status
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from django.http import Http404
+
 
 class ProfileList(APIView):
+    """
+    View to list all profiles in the system.
+    """
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """
+    View to retrieve, update, or delete a profile instance.
+    """
+    serializer_class = ProfileSerializer
+    def get_object(self, pk):
+        try:
+            return Profile.objects.get(pk=pk)
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        profile = self.get_object(pk)
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
