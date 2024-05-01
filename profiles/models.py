4d5e87353de27a442edb89782f2f4329d17c3c5e
Chris Diggle <chrisdiggle8@outlook.com>
2024-05-01 07:14:07 +0000

    added profiles model and migrate

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Extends the default User model by storing additional information 
+    about the user. The profile is automatically created and linked 
+    when a User instance is saved for the first time.
+    """
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(blank=True)
+    phone_number = models.IntegerField(blank=True, null=True)
+    email = models.EmailField(max_length=255, blank=True)
+    profile_picture = models.ImageField(
+        upload_to='images/', default='../default_profile_st6mnc'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
