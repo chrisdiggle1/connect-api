1e27291e77d2bd5b135c8d95bf2b40a5e4d00e75
Chris Diggle <chrisdiggle8@outlook.com>
2024-05-04 14:17:28 +0000

    Add views and url patterns for interested app

--- interested/views.py
+++ interested/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from connect_api.permissions import IsOwnerOrReadOnly
+from interested.models import Interested
+from interested.serializers import InterestedSerializer
 
-# Create your views here.
+
+class InterestedList(generics.ListCreateAPIView):
+    """
+    Lists or creates expressions of interest in events.
+    The perform_create method associates the interest with the logged-in user.
+    """
+    serializer_class = InterestedSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Interested.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class InterestedDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieves or deletes an interest in an event by ID if the user owns it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = InterestedSerializer
+    queryset = Interested.objects.all()
