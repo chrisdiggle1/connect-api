99729ad422931f4584931f93417104a9b74fab63
Chris Diggle <chrisdiggle8@outlook.com>
2024-05-04 16:37:46 +0000

    amend profiles view and serializers to use DRF generics to simplify the views, add searc and filter fields

--- profiles/views.py
+++ profiles/views.py
@@ -1,56 +1,53 @@
-from rest_framework.views import APIView
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from django.http import Http404
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from connect_api.permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
-    View to list all profiles in the system.
+    List all profiles.
+    No create view as profile creation is handled by django signals.
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request})
-        return Response(serializer.data)
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.annotate(
+        events_count=Count('owner__event', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+        attending_count=Count('owner__attending', distinct=True),
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
+    ]
+    search_fields = [
+        "owner__username",
+    ]
+    ordering_fields = [
+        'events_count',
+        'followers_count',
+        'following_count',
+        'attending_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     View to retrieve, update, or delete a profile instance.
     """
     permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.annotate(
+        events_count=Count('owner__event', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+        attending_count=Count('owner__attending', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
-
-    def get_object(self, pk):
-        try:
-            return Profile.objects.get(pk=pk)
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        profile = self.get_object(pk)
-        self.check_object_permissions(self.request, profile)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        profile = self.get_object(pk)
-        self.check_object_permissions(request, profile)
-        serializer = ProfileSerializer(
-        profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        profile = self.get_object(pk)
-        self.check_object_permissions(self.request, profile)
-        profile.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
